cfb20db4cc0b57ee8c9eb2c06608e8d284df4e4d
BAHAR KAYA <bahar.ky092@gmail.com>
2022-09-12 16:12:48 +0100

    updated

---
+++ app.js
@@ -0,0 +1,4 @@
+let input = document.querySelector('#input');
+let add = document.querySelector('#add');
+let clearAll = document.querySelector('#clear');
+let todoAll = document.querySelector('#todo-section');
